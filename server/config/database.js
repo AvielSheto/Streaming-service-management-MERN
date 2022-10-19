2228ada6391857572f0d8cfb3c620607810a6266
AvielSheto <avielsheto@gmail.com>
2022-10-19 15:31:17 +0300

    update

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/cinemaDB", () => {
-  console.log("connected to subscriptionDB database");
+  console.log("connected to cinemaDB database");
 });
 
 
