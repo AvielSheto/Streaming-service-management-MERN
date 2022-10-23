2ba4f5bdd93a4cb8a572995a0c273136475308d7
AvielSheto <avielsheto@gmail.com>
2022-10-23 22:39:32 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,9 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
