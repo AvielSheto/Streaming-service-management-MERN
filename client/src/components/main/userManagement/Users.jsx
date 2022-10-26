0e1e3a64bcf1ee095a124d7f38e32fe7dcf1995b
AvielSheto <avielsheto@gmail.com>
2022-10-26 19:13:08 +0300

    update

--- client/src/components/main/userManagement/Users.jsx
+++ client/src/components/main/userManagement/Users.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+const USERS_URL = 'http://localhost:5000/api/users/'
+
 
 function Users() {
+  const [users, setUsers] = useState()
+
+  const getAllUsers = async () => {
+    const { data } = await axios.get(USERS_URL);
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    getAllUsers()
+
+  }, [])
+  console.log(users);
+
+
   return (
-    <div>users</div>
+    <div>
+      <h1>users</h1>
+      <table class="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+
+
+    </div>
   )
 }
 
