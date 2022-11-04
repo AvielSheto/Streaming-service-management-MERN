01d156de16c0f85b2793515f2f45a2cb07570130
AvielSheto <avielsheto@gmail.com>
2022-11-04 21:50:47 +0200

    update

--- server/controllers/moviesControllers.js
+++ server/controllers/moviesControllers.js
@@ -1,9 +1,47 @@
 const asyncHandler = require('express-async-handler')
 const Movie = require('../models/movieModel')
 
+// @desc    Get all Movies
+// @route   GET /api/movies
+// @access  Public
 const getMovies = asyncHandler(async (req, res) => {
     const movies = await Movie.find({});
     res.status(200).json(movies)
 })
 
-module.exports = { getMovies }
+// @desc    Get all Movies
+// @route   GET /api/movies
+// @access  Public
+const createMovie = asyncHandler(async (req, res) => {
+    const { name, genres, image, premiered } = req.bode
+    if (!name || !genres || !image || !premiered) {
+        res.status(400)
+        throw new Error('please add all field')
+    }
+
+    // Check if movie exists
+    const movieExists = await Movie.findOne({ name })
+    if (movieExists) {
+        res.status(400)
+        throw new Error('movie is already exists')
+    }
+    // Create movie
+    const movie = await Movie.create({
+        name,
+        genres,
+        image,
+        premiered
+    });
+    if (movie) {
+        res.status(201).json({
+            _id: movie.id,
+            name: movie.name,
+        })
+    } else {
+        res.status(400)
+        throw new Error('INvalid movie data')
+    }
+
+})
+
+module.exports = { getMovies, createMovie }
--- server/router/moviesRouter.js
+++ server/router/moviesRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getMovies } = require('../controllers/moviesControllers')
+
+const { getMovies, createMovie } = require('../controllers/moviesControllers')
 
 router.get('/', getMovies)
+router.post('/', createMovie)
 
 module.exports = router
